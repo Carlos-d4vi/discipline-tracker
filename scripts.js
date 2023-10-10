2a3b2fc0094cb4541bbdf800e433aac07fe7068c
Carlos Davi <72183343+Carlos-d4vi@users.noreply.github.com>
2023-10-09 23:07:13 -0300

    Update scripts.js
    bug resolve

--- scripts.js
+++ scripts.js
@@ -1,25 +1,30 @@
-let LocalStorageDays = localStorage.getItem('days')
-updateDays()
-
-document.getElementById('addDayButton').addEventListener('click', function(){
-  let daysUpdated = parseInt(LocalStorageDays)
-  localStorage.setItem('days', daysUpdated += 1)
+if(localStorage.getItem('days') == NaN){
+  localStorage.setItem('days', 0)
+}else{
+  let LocalStorageDays = localStorage.getItem('days')
   updateDays()
-  console.log('A day have been incressed')
-})
-
-document.getElementById('loseDayButton').addEventListener('click', function(){
-  let daysUpdated = parseInt(LocalStorageDays)
-  if(daysUpdated > 0){
-    localStorage.setItem('days', daysUpdated -= 1)
-    console.log('A day have been decressed')
-  }else{
-    alert('Não é possível retirar mais dias. Tente começar hoje!')
+  
+  document.getElementById('addDayButton').addEventListener('click', function(){
+    let daysUpdated = parseInt(LocalStorageDays)
+    localStorage.setItem('days', daysUpdated += 1)
+    updateDays()
+    console.log('A day have been incressed')
+  })
+  
+  document.getElementById('loseDayButton').addEventListener('click', function(){
+    let daysUpdated = parseInt(LocalStorageDays)
+    if(daysUpdated > 0){
+      localStorage.setItem('days', daysUpdated -= 1)
+      console.log('A day have been decressed')
+    }else{
+      alert('Não é possível retirar mais dias. Tente começar hoje!')
+    }
+    updateDays()
+  })
+  
+  function updateDays(){
+    document.getElementById('Days').textContent = localStorage.getItem('days')
+    LocalStorageDays = localStorage.getItem('days')
   }
-  updateDays()
-})
-
-function updateDays(){
-  document.getElementById('Days').textContent = localStorage.getItem('days')
-  LocalStorageDays = localStorage.getItem('days')
 }
+
