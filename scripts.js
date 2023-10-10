80684a69c5ef5711d2162594ba9a41e83173fd1f
Carlos Davi <72183343+Carlos-d4vi@users.noreply.github.com>
2023-10-09 23:22:54 -0300

    Update scripts.js
    bug solved

--- scripts.js
+++ scripts.js
@@ -1,30 +1,30 @@
-if(localStorage.getItem('days') == NaN){
+let LocalStorageDays = localStorage.getItem('days')
+
+if(localStorage.getItem('days') === null || isNaN(LocalStorageDays)){
   localStorage.setItem('days', 0)
-}else{
-  let LocalStorageDays = localStorage.getItem('days')
-  updateDays()
-  
-  document.getElementById('addDayButton').addEventListener('click', function(){
-    let daysUpdated = parseInt(LocalStorageDays)
-    localStorage.setItem('days', daysUpdated += 1)
-    updateDays()
-    console.log('A day have been incressed')
-  })
-  
-  document.getElementById('loseDayButton').addEventListener('click', function(){
-    let daysUpdated = parseInt(LocalStorageDays)
-    if(daysUpdated > 0){
-      localStorage.setItem('days', daysUpdated -= 1)
-      console.log('A day have been decressed')
-    }else{
-      alert('Não é possível retirar mais dias. Tente começar hoje!')
-    }
-    updateDays()
-  })
-  
-  function updateDays(){
-    document.getElementById('Days').textContent = localStorage.getItem('days')
-    LocalStorageDays = localStorage.getItem('days')
-  }
 }
 
+updateDays()
+
+document.getElementById('addDayButton').addEventListener('click', function(){
+  let daysUpdated = parseInt(LocalStorageDays)
+  localStorage.setItem('days', daysUpdated += 1)
+  updateDays()
+  console.log('A day have been incressed')
+})
+
+document.getElementById('loseDayButton').addEventListener('click', function(){
+  let daysUpdated = parseInt(LocalStorageDays)
+  if(daysUpdated > 0){
+    localStorage.setItem('days', daysUpdated -= 1)
+    console.log('A day have been decressed')
+  }else{
+    alert('Não é possível retirar mais dias. Tente começar hoje!')
+  }
+  updateDays()
+})
+
+function updateDays(){
+  document.getElementById('Days').textContent = localStorage.getItem('days')
+  LocalStorageDays = localStorage.getItem('days')
+}
