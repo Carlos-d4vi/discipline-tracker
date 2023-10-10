9dabebf2752a458d55ce2ac2ebed5b15ab6c6e10
Carlos Davi <72183343+Carlos-d4vi@users.noreply.github.com>
2023-10-09 23:01:28 -0300

    files upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let LocalStorageDays = localStorage.getItem('days')
+updateDays()
+
+document.getElementById('addDayButton').addEventListener('click', function(){
+  let daysUpdated = parseInt(LocalStorageDays)
+  localStorage.setItem('days', daysUpdated += 1)
+  updateDays()
+  console.log('A day have been incressed')
+})
+
+document.getElementById('loseDayButton').addEventListener('click', function(){
+  let daysUpdated = parseInt(LocalStorageDays)
+  if(daysUpdated > 0){
+    localStorage.setItem('days', daysUpdated -= 1)
+    console.log('A day have been decressed')
+  }else{
+    alert('Não é possível retirar mais dias. Tente começar hoje!')
+  }
+  updateDays()
+})
+
+function updateDays(){
+  document.getElementById('Days').textContent = localStorage.getItem('days')
+  LocalStorageDays = localStorage.getItem('days')
+}
